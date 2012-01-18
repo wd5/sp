a791aa0c7d34e79248a710ad43f4233fab95feec
wd5 <developer@wd5.ru>
2012-01-18 16:59:05 +0400

    modified:   models.py 	modified:   models.pyc

--- models.py
+++ models.py
@@ -3,11 +3,13 @@ from django.db import models
  
 # Create your models here.
 class SpPages(models.Model):
-    title = models.CharField(max_length = 300, verbose_name= (u'Название ПС')) # заголовок для поисковика
-    description = models.CharField(max_length = 255, blank=True, verbose_name=(u'Описание ПС')) # описание для поисковиков
-    keywords = models.CharField(max_length = 255, blank=True, verbose_name=(u'Ключевые слова ПС')) # описание для поисковиков
-    header = models.TextField(verbose_name=(u'Название для людей')) # заголовок для новости
-    content = models.TextField(verbose_name=(u'Cодержание')) #  подробное содержимое новости
+    title = models.CharField(max_length = 300, verbose_name= (u'Название ПС'))
+    description = models.CharField(max_length = 255, blank=True, verbose_name=(u'Описание ПС'))
+    keywords = models.CharField(max_length = 255, blank=True, verbose_name=(u'Ключевые слова ПС')) 
+    header = models.TextField(verbose_name=(u'Название для людей')) 
+    content = models.TextField(verbose_name=(u'Cодержание'))
+    menuname = models.CharField(max_length = 300, verbose_name= (u'Название в меню'))
+    aliasname = models.CharField(max_length = 300, verbose_name= (u'ЧПУ')) 
     catkey = models.ForeignKey('SpPages', verbose_name=(u'Раздел'), blank=True, null=True)
  
     def __unicode__(self):
