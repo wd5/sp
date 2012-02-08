86f4c5da35454fadf6086376c7276a690a9158db
wd5 <developer@wd5.ru>
2012-02-08 23:58:43 +0400

    Добавлены поля для введения, даты начала и отмены публикации, порядок сортировки, поле для вложений и миниатюры. Первые буквы переменных переведены в Верхний регистр

--- models.py
+++ models.py
@@ -3,16 +3,25 @@ from django.db import models
  
 # Create your models here.
 class SpPage(models.Model):
-    title = models.CharField(max_length = 300, verbose_name= (u'Название ПС'))
-    description = models.CharField(max_length = 255, blank=True, verbose_name=(u'Описание ПС'))
-    keywords = models.CharField(max_length = 255, blank=True, verbose_name=(u'Ключевые слова ПС')) 
-    header = models.CharField(max_length = 300, verbose_name=(u'Название для людей'), blank=True) 
-    content = models.TextField(verbose_name=(u'Cодержание'))
-    menuname = models.CharField(max_length = 300, verbose_name= (u'Название в меню'), blank=True)
-    menustatus = models.BooleanField(verbose_name=(u'Отображать в меню'))
-    aliasname = models.CharField(max_length = 300, verbose_name= (u'ЧПУ')) 
-    catkey = models.ForeignKey('SpPage', verbose_name=(u'Раздел'), blank=True, null=True)
+    Title = models.CharField(max_length = 300, verbose_name= (u'Название ПС'))
+    Header = models.CharField(max_length = 300, verbose_name=(u'Название для людей'), blank=True, null=True)
+    Description = models.CharField(max_length = 255, verbose_name=(u'Описание ПС'), blank=True, null=True)
+    keywords = models.CharField(max_length = 255, verbose_name=(u'Ключевые слова ПС'), blank=True, null=True)
+    IntroText = models.TextField(verbose_name=(u'Аннотация (введение)'), blank=True, null=True)
+    Content = models.TextField(verbose_name=(u'Cодержание'))
+    MenuName = models.CharField(max_length = 300, verbose_name= (u'Название в меню'), blank=True, null=True)
+    MenuStatus = models.BooleanField(verbose_name=(u'Отображать в меню'))
+    AliasName = models.CharField(max_length = 300, verbose_name= (u'ЧПУ')) 
+    CatKey = models.ForeignKey('SpPage', verbose_name=(u'Раздел'), blank=True, null=True)
+    VisibleStatus = models.BooleanField(verbose_name=(u'Опубликован'))
+    SearchEnginesStatus = models.BooleanField(verbose_name=(u'Доступ для ПС'))
+    DeleteStatus = models.BooleanField(verbose_name=(u'Удален в корзину'))
+    PubDateStart = models.DateTimeField(verbose_name=(u'Дата публикации'), blank=True, null=True)
+    PubliDateStop = models.DateTimeField(verbose_name=(u'Дата отмены публикации'), blank=True, null=True)
+    Position = models.IntegerField(verbose_name=(u'Позиция или приоритет'), blank=True, null=True)
+    Thumbs = models.ImageField(upload_to='thumbs', verbose_name=(u'Миниатюра'), blank=True, null=True)
+    Attachment = models.FileField(upload_to='files', verbose_name=(u'Прикрепленный файл'), blank=True, null=True)
  
     def __unicode__(self):
-        result = self.title
+        result = self.Title
         return result
